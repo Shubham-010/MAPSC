7f9b9a5c52fd3d62e9a0b5374f64bfc842d65440
ShubhamDC <shubhamchikane0109@gmail.com>
2025-06-30 06:56:59 +0530

    MAPSC lETS GO!!

---
+++ script.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
+//!Navigating to user's current location and logging it
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      //   console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
+
+      map = L.map('map').setView([latitude, longitude], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      //! Add Marker on click
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        console.log('mapEvent', mapEvent);
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert(
+        'Could not get your position. Please allow location access in your browser settings.'
+      );
+    },
+    {}
+  );
+}
+//! Form input validation
+//! Form submission
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('New workout')
+    .openPopup();
+});
+
+//! Toggle elevation/cadence input fields based on workout type
+inputType.addEventListener('change', function () {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.value = inputElevation.value = '';
+});
